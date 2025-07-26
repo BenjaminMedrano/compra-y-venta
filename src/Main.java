eca6dbcbf4f46719c0879cb644a6509865c1e27c
BenjaminMedrano <joel.64jo@gmail.com>
2025-07-26 15:38:31 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import model.Usuario;
+import model.Producto;
+import service.PublicacionService;
+import service.CompraService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bienvenido al Marketplace");
+        Usuario user = new Usuario("benja", "benja@mail.com");
+        PublicacionService pubService = new PublicacionService();
+        CompraService compraService = new CompraService();
+
+        Producto p1 = pubService.publicarProducto(user, "Laptop usada", 2000);
+        pubService.mostrarProductos();
+
+        compraService.comprarProducto(user, p1);
     }
 }
